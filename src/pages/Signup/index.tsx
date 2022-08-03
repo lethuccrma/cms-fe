36ea37562acd49753056c6796c52eb552112f7f1
Thuc Le <lethuc.crma@gmail.com>
2022-08-03 14:48:33 +0700

    feat: style signup page

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,14 +1,13 @@
-import { Button, Col, Form, Input, Row, Select, Space, Typography } from 'antd';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-import './index.css';
+import React from 'react';
+import { Button, Col, Form, Input, Row, Select, Typography } from 'antd';
+import { useEffect, useState } from 'react';
 import UnauthorizedAPI from '../../apis/unauthorized';
 import { SIGNUP } from '../../configs/server';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { AuthState } from '../../redux/auth/auth.slice';
 import { USER_ROLE } from '../../types/IUser';
+import { SignupStyled } from './styled';
 
 const {Option} = Select;
 
@@ -56,14 +55,29 @@ export default function index() {
   };
 
   return (
-    <div className="flex h-screen w-screen justify-center	 items-center">
+    <SignupStyled className="flex h-screen w-screen">
+      <div className='left-side-container overflow-hidden'>
+        <div className='poster-img-container'>
+          <div className='banner-txt-container'>
+            <div>Design APIs fast</div>
+            <div className='mt-2'>Manage content easily.</div>
+          </div>
+          <img className='poster-img' src='/poster-image.png'/>
+        </div>
+        <div className='large-oval absolute' style={{top: 20, right: 20}} />
+        <div className='small-oval absolute' style={{top: 200, right: 200}} />
+        <div className='large-oval absolute' style={{left: 0, bottom: 50}} />
+        <div className='small-oval absolute' style={{left: 200, bottom: -50}} />
+      </div>
+      <div className='right-side-container'>
+        <div className='signup-title'>Signup new CMS+ account</div>
       <Form
         form={form}
         name="register"
         onFinish={onFinish}
         initialValues={{ remember: true }}
         scrollToFirstError
-        className="signup-form min-w-[40%]"
+        className="signup-form"
       >
         <Row gutter={24}>
           <Col span={12}>
@@ -177,9 +191,10 @@ export default function index() {
           >
             Sign up
           </Button>
-          Or <a href="/login">log in!</a>
         </Form.Item>
       </Form>
-    </div>
+      <a className='register-link' href="/login">Login now</a>
+      </div>
+    </SignupStyled>
   );
 }
---
+++ src/pages/Signup/styled.ts
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+
+export const SignupStyled = styled.div`
+  .left-side-container {
+    display: flex;
+    background-image: radial-gradient(circle at 19% 7%, #f5f8ff, #e5e9ff 109%);
+    flex: 2;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  .poster-img {
+    width: 600px;
+    height: auto;
+  }
+  .poster-img-container {
+    position: relative;
+  }
+  .banner-txt-container {
+    position: absolute;
+    left: -50px;
+    top: -80px;
+
+    font-family: Montserrat;
+    font-size: 36px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #4f4f4f;
+  }
+  .large-oval {
+    width: 188px;
+    height: 188px;
+    border-radius: 50%;
+    background-color: #fff;
+  }
+  .small-oval {
+    width: 126px;
+    height: 126px;
+    border-radius: 50%;
+    background-color: #fff;
+  }
+  .right-side-container {
+    flex: 1;
+    display: flex;
+    background-color: #272727;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .signup-title{
+    font-family: Montserrat;
+    font-size: 27px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: #fff;
+  }
+  .signup-form {
+    width: 100%;
+    max-width: 300px;
+    margin-top: 100px;
+  }
+  .signup-form-button {
+    width: 100%;
+    height: 40px;
+    border-radius: 6px;
+    background-image: radial-gradient(circle at 0 0, #9d70ff, #633dff);
+    border: none;
+  }
+  .input-label {
+    opacity: 0.5;
+    font-family: Montserrat;
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #fff;
+    margin-bottom: 10px;
+  }
+  .register-link {
+    text-decoration: underline;
+    opacity: 0.5;
+    font-family: Montserrat;
+    font-size: 14px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #fff;
+  }
+`;
